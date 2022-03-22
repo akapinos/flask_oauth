8edd6ccf84e88b515f3d304f9aadbfbf37fca514
akapinos <art.harakiri@gmail.com>
2022-03-22 23:33:40 +0300

    Add /login, /logout, /about endpoints

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import datetime
 import inspect
+import json
 import os
 import requests
 import sys
+import sqlite3
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, url_for
+from flask import (Flask, redirect, render_template, Response, request,
+                   url_for)
+from flask_login import (LoginManager, current_user, login_required,
+                         login_user, logout_user)
+from oauthlib.oauth2 import WebApplicationClient
 from typing import Optional
+
+from db import init_db_command
+from user import User
 
 APP_ENDPOINTS = [
     'login', 'logout', 'index', 'list_city', 'city_date', 'about', 'useragent'
@@ -15,14 +24,41 @@ APP_ENDPOINTS = [
 load_dotenv()
 
 OWM_API_KEY = os.environ.get('OWM_API_KEY')
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', None)
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', None)
+GOOGLE_DISCOVERY_URL = (
+    'https://accounts.google.com/.well-known/openid-configuration')
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(24)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    pass  # Assume it's already been created
+
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 def get_endpoints(endp_name: str) -> dict:
-    endpoints = [e for e in APP_ENDPOINTS if e != endp_name]
+    excluded_endpoints = [endp_name]
     endpoints_dict = {}
     mod = sys.modules[__name__]
+
+    if current_user.is_authenticated:
+        excluded_endpoints.append('login')
+    else:
+        excluded_endpoints.append('logout')
+
+    endpoints = [e for e in APP_ENDPOINTS if e not in excluded_endpoints]
 
     for e in endpoints:
         func = getattr(mod, e)
@@ -82,14 +118,75 @@ def index() -> str:
     return render_template('index.html', endpoints=endpoints)
 
 
+def get_google_provider_cfg() -> Response:
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
 @app.route('/login')
-def login() -> str:
-    return 'login'
+def login() -> Response:
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg['authorization_endpoint']
+
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + '/callback',
+        scope=['openid', 'email', 'profile'],
+    )
+    return redirect(request_uri)
+
+
+@app.route('/login/callback')
+def callback():
+    code = request.args.get('code')
+
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg['token_endpoint']
+
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    if userinfo_response.json().get('email_verified'):
+        unique_id = userinfo_response.json()['sub']
+        users_email = userinfo_response.json()['email']
+        picture = userinfo_response.json()['picture']
+        users_name = userinfo_response.json()['given_name']
+    else:
+        return 'User email not available or not verified by Google.', 400
+
+    user = User(id_=unique_id,
+                name=users_name,
+                email=users_email,
+                profile_pic=picture)
+
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    login_user(user)
+
+    return redirect(url_for('index'))
 
 
 @app.route('/logout')
-def logout() -> str:
-    return 'logout'
+def logout() -> Response:
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/list/<city>')
@@ -176,7 +273,15 @@ def city_date(city=None, date=None) -> str:
 
 @app.route('/about')
 def about() -> str:
-    return 'about'
+    if current_user.is_authenticated:
+        return ('<p>Hello, {}! You\'re logged in! Email: {}</p>'
+                '<div><p>Google Profile Picture:</p>'
+                '<img src="{}" alt="Google profile pic"></img></div>'
+                '<a class="button" href="/logout">Logout</a>'.format(
+                    current_user.name, current_user.email,
+                    current_user.profile_pic))
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
 
 
 @app.route('/useragent')
@@ -188,5 +293,5 @@ def useragent() -> str:
             f'<p>Browser: {browser}</p>')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc')
